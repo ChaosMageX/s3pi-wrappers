8af3967dcbea1985962cd23e9bf5b1c447eff3a2
AndrewTavera <andrewtavera@gmail.com>
2010-10-11 15:04:27 +0000

    BoneTool: Uses inverse function to calc matrix now
    git-svn-id: svn://localhost/tmp/s3pi-wrappers/trunk@96 6325f554-a1a8-4be4-9c0a-7068805f9fcb

--- Helpers/BoneTool/GrannyBoneControl.cs
+++ Helpers/BoneTool/GrannyBoneControl.cs
@@ -39,26 +39,27 @@ namespace s3piwrappers.BoneTool
         {
             Matrix m = new Matrix(tcLocalTransform.Orientation);
             m.Translation = tcLocalTransform.Position;
+            Matrix mi = m.GetInverse();
             Matrix4x4 g = mValue.InverseWorld4X4;
-            g.M0.X = (float)m.M11;
-            g.M0.Y = (float)m.M12;
-            g.M0.Z = (float)m.M13;
-            g.M0.W = (float)m.M14;
+            g.M0.X = (float)mi.M11;
+            g.M0.Y = (float)mi.M12;
+            g.M0.Z = (float)mi.M13;
+            g.M0.W = (float)mi.M14;
 
-            g.M1.X = (float)m.M21;
-            g.M1.Y = (float)m.M22;
-            g.M1.Z = (float)m.M23;
-            g.M1.W = (float)m.M24;
+            g.M1.X = (float)mi.M21;
+            g.M1.Y = (float)mi.M22;
+            g.M1.Z = (float)mi.M23;
+            g.M1.W = (float)mi.M24;
 
-            g.M2.X = (float)m.M31;
-            g.M2.Y = (float)m.M32;
-            g.M2.Z = (float)m.M33;
-            g.M2.W = (float)m.M34;
+            g.M2.X = (float)mi.M31;
+            g.M2.Y = (float)mi.M32;
+            g.M2.Z = (float)mi.M33;
+            g.M2.W = (float)mi.M34;
 
-            g.M3.X = (float)m.M41;
-            g.M3.Y = (float)m.M42;
-            g.M3.Z = (float)m.M43;
-            g.M3.W = (float)m.M44;
+            g.M3.X = (float)mi.M41;
+            g.M3.Y = (float)mi.M42;
+            g.M3.Z = (float)mi.M43;
+            g.M3.W = (float)mi.M44;
             
         }
         void tcLocalTransform_Changed(object sender, EventArgs e)
