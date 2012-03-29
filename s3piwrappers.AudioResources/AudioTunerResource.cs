34972f3ea0af294dd8247cf36f5e3953fdbe4c1e
ChaosMageX <startrektommy@gmail.com>
2012-03-29 08:33:03 +0000

    + Added SoundProperty enumeration to AudioTunerResource, based on uint constants discovered in the Sims3.SimIFace.Audio class in the SimIFace.dll that comes with the Create A World Tool. + Added a corresponding Name property to AudioTunerResource.DataBlock class that changes the mId value.
    git-svn-id: svn://localhost/tmp/s3pi-wrappers/trunk@180 6325f554-a1a8-4be4-9c0a-7068805f9fcb

--- s3piwrappers.AudioResources/AudioTunerResource.cs
+++ s3piwrappers.AudioResources/AudioTunerResource.cs
@@ -13,6 +13,84 @@ namespace s3piwrappers
 {
     public class AudioTunerResource : AResource
     {
+        public enum SoundProperty : uint
+        {
+            AddGroups = 0xf58ca0be,
+            Age = 0x489c3762,
+            Aggregate = 0x5a32a0ce,
+            AtmosphericCurve = 0xbdc98ce5,
+            AtmosphericHiPassCurve = 0x8adce22f,
+            AttenuatedGain = 0x6325c989,
+            Attenuation = 0x1e4db1eb,
+            AttenuationCurve = 0x6e3d22e0,
+            Codec = 0xba03d0cd,
+            DacOutputSampleRate = 0x6a371e0,
+            DefaultToMaster = 0x6b16ae3d,
+            Delay = 0x15525baa,
+            DopplerCurve = 0x9bfc37ac,
+            Duration = 0xb1f42539,
+            EffectiveGain = 0xc2d1f579,
+            EmitterType = 0x6610c2bd,
+            FadeIn = 0x98ac8996,
+            FadeOut = 0xe8ab99b9,
+            Feedback = 0xfc047eec,
+            Gain = 0x25df0108,
+            Groups = 0x7cb81a35,
+            HardStop = 0x222b8006,
+            HighPass = 0xa26a765f,
+            IgnorePauseGain = 0xd1daef42,
+            Is3d = 0x12d2a4d4,
+            IsLooped = 0x6dd08218,
+            IsVirtual = 0xba134192,
+            LoopPlaylist = 0x3c10c7b9,
+            LowPass = 0x647a7836,
+            MaxDistance = 0x3593710,
+            MaxGainChange = 0xc742ceb8,
+            MinDistance = 0xf616b72,
+            MinPerformanceLevel = 0xb2550953,
+            MuteAll = 0x899eb157,
+            NoteDuration = 0xcd928a9,
+            NoteNumber = 0x66ef9aba,
+            NoteVelocity = 0xe4dd1818,
+            Pan = 0x4a77693a,
+            PanDistance = 0x6eb7a717,
+            PanSize = 0xce78d695,
+            PanTwist = 0xa4d2160b,
+            Pause = 0xb85523e5,
+            PerformanceLevel = 0x6df5822d,
+            PickupIndex = 0xed1f36d5,
+            Pitch = 0x71bc3009,
+            PitchShiftModifier = 0x779a5ff4,
+            Polyphony = 0xbaf7f4f9,
+            PolyphonyMode = 0x7f28acc,
+            PositionX = 0x50c5d67,
+            PositionY = 0x50c5d66,
+            PositionZ = 0x50c5d65,
+            PreFade = 0x6257eb2a,
+            PrimitiveHashes = 0xe39eb081,
+            Primitives = 0x3da0a727,
+            Priority = 0x393f7f7d,
+            Probability = 0x8fc9308e,
+            RandomPitch = 0x56e7c242,
+            RemoveGroups = 0x14043549,
+            RingModFreq = 0x703d682b,
+            Rolloff = 0x2406a047,
+            SampleLength = 0xd24d4aff,
+            SamplePosition = 0x893f8476,
+            Samples = 0x701ed91e,
+            SeekTime = 0x14f9bdf6,
+            Send = 0x2fe09c83,
+            Skip = 0x310330cc,
+            StartDelay = 0x29e8b9f8,
+            StreamBufferReadSize = 0xb03a6504,
+            StreamBufferSize = 0x3c2b4ea4,
+            SurroundOn = 0xfe3c587c,
+            Symbols = 0x4b2a3424,
+            TimeInvariantPitch = 0x82beafe0,
+            WetLevel = 0x49eb68db,
+            WetLevelCurve = 0x173c2710
+        }
+
         public class BlockList : DependentList<DataBlock>
         {
 
@@ -531,11 +609,16 @@ namespace s3piwrappers
                 Parse(s);
             }
 
+            public SoundProperty Name
+            {
+                get { return (SoundProperty)mId; }
+                set { if (mId != (uint)value) { mId = (uint)value; OnElementChanged(); } }
+            }
 
             public UInt32 Id
             {
                 get { return mId; }
-                set { mId = value; if (mId != value) { mId = value; OnElementChanged(); } }
+                set { if (mId != value) { mId = value; OnElementChanged(); } }
             }
             public ushort RepCode
             {
